c03e94d734239bb2395f6fbf62cac205a845bde1
Misha Padalka <misha.cn.ua@gmail.com>
2015-03-05 23:23:39 +0200

    ticket:4 Implement phone filter and use it

--- django_angular_backend/static/js/filters/phone.js
+++ django_angular_backend/static/js/filters/phone.js
@@ -4,6 +4,9 @@ angular.module('test.phoneFilter', [])
 
 .filter('phone', function() {
     return function(input) {
+        if (input.slice(0, 2) === '+1') {
+            input = '(USA) ' + input;
+        }
         return input;
     };
 });
